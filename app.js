920a6c96d2879e5c9cba49c0a2287682af414864
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-10 22:37:34 +0200

    Modules

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const amount = 12
 
+
 if(amount < 10){
     console.log('small number');
 }
@@ -8,3 +9,15 @@ else{
 }
 
 console.log(`hey it's my first node app!!!`);
+
+// CommonJS, every file is module (by default)
+// Module - Encapsulated Code (only share minimum)
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+console.log(names)
+
+
+ sayHi('susan')
+ sayHi(names.john)
+ sayHi(names.peter)
