a9248ea7fa9f817aa7ffc7b67e4cc308c72b3f11
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-10 22:55:12 +0200

    Another update

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ console.log(`hey it's my first node app!!!`);
 
 const names = require('./4-names');
 const sayHi = require('./5-utils');
-console.log(names)
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade');
 
-
- sayHi('susan')
- sayHi(names.john)
- sayHi(names.peter)
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
